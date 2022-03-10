939822715a5137d65cd1eedbc60c66316d7bc664
bbaekddo <bbaekddo100@naver.com>
2022-03-10 22:15:41 +0900

    Remake pages, create option

--- main.js
+++ main.js
@@ -1,10 +1,213 @@
 const express = require('express');
+const tp = require('./lib/template');
+const database = require("./lib/db");
+const qs = require("querystring");
+const sn = require("sanitize-html");
 const app = express();
 
+let pageTitle = "";
+const control = `
+            <p>
+                <a href="/create">create</a>
+                <a href="/update">update</a>
+                <form action="/delete" method="post">
+                    <input type="hidden" name="id" value="${pageTitle}">
+                    <input type="submit" value="delete">
+                </form>
+            </p>
+            `;
+
+app.get('/pages/:pageTitle', (req, res) => {
+    pageTitle = req.params.pageTitle;
+    let topicList = [];
+    
+    /*const control = `
+            <p>
+                <a href="/create">create</a>
+                <a href="/update">update</a>
+                <form action="/delete" method="post">
+                    <input type="hidden" name="id" value="${pageTitle}">
+                    <input type="submit" value="delete">
+                </form>
+            </p>
+            `;*/
+    
+    database.query('SELECT * FROM topic', (error, topics) => {
+        if (error) {
+            throw error;
+        } else{
+            for(let i=0; i<topics.length; i++){
+                topicList.push(topics[i].title);
+            }
+            database.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE title=?`, [pageTitle], (error, data) => {
+                //language=HTML
+                const desc = `
+                    <p>${data[0].description} by ${data[0].name}</p>
+                    `;
+                
+                if (error){
+                    throw error;
+                } else{
+                    res.send(tp.html(pageTitle, topicList, desc, control));
+                }
+            });
+        }
+    });
+});
+
+app.get('/create', (req, res) => {
+    pageTitle = "Create Index";
+    let topicList = [];
+    
+    database.query('SELECT * FROM topic', (error, topics) => {
+        if (error) {
+            throw error;
+        } else{
+            for(let i=0; i<topics.length; i++){
+                topicList.push(topics[i].title);
+            }
+    
+            database.query(`SELECT id, name FROM author`, (error, authors) => {
+                if (error) {
+                    throw error;
+                } else{
+                    let authorList = `<select name="authors">`;
+                    for (let author of authors) {
+                        authorList += `<option value="${author.name}">${author.name}</option>`;
+                    }
+                    authorList += `</select>`;
+                    
+                    //language=HTML
+                    const description = `
+                        <form action="/create" method="post">
+                            <p>
+                                <input type="text" name="newTitle" placeholder="Title">
+                            </p>
+                            <p>
+                                <textarea name="description" placeholder="Description"></textarea>
+                            </p>
+                            <p>
+                                ${authorList}
+                            </p>
+                            <p>
+                                <input type="submit" name="submit">
+                            </p>
+                        </form>
+                    `;
+                    
+                    res.send(tp.html(pageTitle, topicList, description, control));
+                }
+            });
+        }
+    });
+});
+
+app.post('/create', (req, res) => {
+    let createBody = '';
+    
+    req.on('data', (data) => {
+        createBody += data;
+    });
+    console.log(createBody);
+    req.on('end', () => {
+        const post = qs.parse(createBody);
+        const title = post.newTitle;
+        const saniDesc = sn(post.description, {
+            allowedTags : ['i', 'b']
+        });
+        const authorName = post.authors;
+        let authorId;
+        
+        database.query(`SELECT id, name FROM author`, (error, authors) => {
+            if (error) {
+                throw error;
+            } else {
+                for (const list of authors) {
+                    if (list.name === authorName) {
+                        authorId = list.id;
+                    }
+                }
+                
+                database.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?, ?, NOW(), ?)`,
+                    [title, saniDesc, authorId], (error) => {
+                        if (error){
+                            throw error;
+                        } else{
+                            res.redirect(`/pages/${title}`);
+                        }
+                    });
+            }
+        });
+    });
+});
+
+app.get('/update', (req, res) => {
+    let topicList = [];
+    
+    database.query('SELECT * FROM topic', (error, topics) => {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i < topics.length; i++) {
+                topicList.push(topics[i].title);
+            }
+            
+            database.query(`SELECT * FROM topic WHERE title=?`, [pageTitle], (error, data1) => {
+                if (error) {
+                    throw error;
+                } else{
+                    database.query(`SELECT * FROM author`, (error, data2) => {
+                        if (error) {
+                            throw error;
+                        } else{
+                            const description = `
+                                    <form action="/process_update" method="post">
+                                        <p>
+                                            <input type="text" name="id" placeholder="Update Title">
+                                        </p>
+                                        <p>
+                                            <input type="text" name="textline" value="${data1[0].title}" readonly>
+                                        </p>
+                                        <p>
+                                            <textarea name="description">${data1[0].description}</textarea>
+                                        </p>
+                                        <p>
+                                            ${tp.selec(data2, data1[0].author_id)}
+                                        </p>
+                                        <p>
+                                            <input type="submit" name="submit">
+                                        </p>
+                                    </form>
+                                    `;
+                            
+                            response.writeHead(200);
+                            response.end(tp.html(data1[0].title, topicList, description, options));
+                        }
+                    });
+                }
+            });
+        }
+    });
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    pageTitle = "Welcome";
+    let topicList = [];
+    const description = "Hello Node.js!";
+    
+    database.query('SELECT * FROM topic', (error, topics) => {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i < topics.length; i++) {
+                topicList.push(topics[i].title);
+            }
+            
+            res.send(tp.html(pageTitle, topicList, description, `<p><a href="/create">create</a></p>`));
+        }
+    });
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000');
+    console.log(`Example app listening on port 3000`);
 });
